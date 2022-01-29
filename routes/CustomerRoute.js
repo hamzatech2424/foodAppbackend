68bc93a0604a2832dcbedf8466cad889bd2cd956
Hamza Shafiq <64663350+hamzatech2424@users.noreply.github.com>
2022-01-29 14:09:27 +0500

    Update CustomerRoute.js

--- routes/CustomerRoute.js
+++ routes/CustomerRoute.js
@@ -130,7 +130,7 @@ router.post("/login", async (req, res) => {
         return res.status(200).json({ success: true, message: "Login Successfully", user: user, token: token })
     }
     else {
-        return res.status(400).send("InCorrect Password")
+        return res.status(400).json({ success: false, message: "Password Incorrect" })
     }
 })
 
